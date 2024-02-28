18e3011de37cb1c220fcd683e3e8941ec852f7d9
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2024-02-28 19:39:59 -0300

    Add project files.

---
+++ Exercise03/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03.Entities.Enums
+{
+    enum OrderStatus: int
+    {
+        Pending_Payment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
---
+++ Exercise03/Entities/Order.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Exercise03.Entities.Enums;
+using System.Globalization;
+
+namespace Exercise03.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Item { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            Item.Add(orderItem);
+        }
+        public void RemoveItem(OrderItem orderItem)
+        {
+            Item.Remove(orderItem);
+        }
+        public double Total()
+        {
+            double sum = 0;
+            foreach(OrderItem item in Item)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Item)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
